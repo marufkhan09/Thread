96e73cc93a7f76255d0409b922a675454fffbff5
marufkhan09 <maruf1609@gmail.com>
2023-11-27 16:47:38 +0600

    Initial Commit

---
+++ thread/threadApp.swift
@@ -0,0 +1,17 @@
+//
+//  threadApp.swift
+//  thread
+//
+//  Created by Maruf Khan on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct threadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
